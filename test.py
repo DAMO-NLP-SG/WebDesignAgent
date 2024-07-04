772dd8519391aabd9fc0fc2875bcaa9213e3d2be
jianghuyihei <longli@zju.edu.cn>
2024-07-04 16:56:14 +0800

    delete test

--- test.py
+++
@@ -1,6 +0,0 @@
-from prompts import *
-
-print(get_write_original_website_prompt("test","test",language="zh",local_img_storage = ["test"]))
-# get_refine_prompt("test","test",language="zh",local_img_storage = ["test"])
-# print(get_refine_page_prompt("test","test"))
-# print(get_page_complete_prompt("test","test"))
